a37633c9e5eed50e5ca6609ae1c2a52b82b04e69
Sara <127508066+sarapanza01@users.noreply.github.com>
2024-05-30 19:51:50 +0200

    Update LoopFusion.cpp
    Aggiunto il punto 2 per il quarto assignment

--- Assignemnt_4/LoopFusion.cpp
+++ Assignemnt_4/LoopFusion.cpp
@@ -2,8 +2,24 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/Dominators.h"
 #include "llvm/Analysis/PostDominators.h"
+#include "llvm/Analysis/ScalarEvolution.h"
 
 using namespace llvm;
+//Il secondo punto mi chiede di vedere se i due loop hanno lo stesso numero di iterazioni
+int getLoopIterations(ScalarEvolution &SE, Loop &L)
+{
+        //conto tutti i rami backedge per sapere quante volte viene richiamato il loop
+        const SCEV *BackedgeTakenCount = SE.getBackedgeTakenCount(L);
+        if (const SCEVConstant *C = dyn_cast<SCEVConstant>(BackedgeTakenCount)) 
+        {
+                return C->getValue();
+        } 
+        else 
+        {
+            return 0;
+        }
+}
+
 //Vorrei iniziare a lavorare sul punto 3, creando una funzione apposita che verifichi se due loop sono 
 //Control Flow Equivalent. Per scrivere l'algoritmo che fa questa operazione, mi baserò su 
 //quanto scritto nelle slide del prof
@@ -29,18 +45,26 @@ FunctionAnalysisManager &AM) {
         LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
         DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
         PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
+        //Per il punto 2
+        ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
 
         for (auto *L0 : LI)
         {
+                //per controllare se il punto 2 funziona
+                outs() << "Il loop fa " << getLoopIterations(SE,L0) << " iterazioni\n";
                 for (auto *L1 : LI)
                 {
-                        if (isControlFlowEquivalent(DT,PDT,L0,L1))
+                        //Ora applico il secondo punto
+                        if (getLoopIterations(SE,L0) == getLoopIterations(SE,L1))
                         {
-                                outs() << "I loop sono entrambi eseguiti\n";
-                        }
-                        else
-                        {
-                                outs() << "I loop non sono Control Flow Equivalent\n";
+                                if (isControlFlowEquivalent(DT,PDT,L0,L1))
+                                {
+                                        outs() << "I loop sono entrambi eseguiti\n";
+                                }
+                                else
+                                {
+                                        outs() << "I loop non sono Control Flow Equivalent\n";
+                                }
                         }
                 }
         }
